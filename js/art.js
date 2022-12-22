edc2d10459f11239b666b5411b7289316d9485e6
nedoxff <nedoprogrammer0xff@gmail.com>
2022-12-22 20:59:25 +0300

    🥴

---
+++ js/art.js
@@ -0,0 +1,18 @@
+﻿$(() => {
+    createCard("christmas_happier.png", "this was my discord profile picture for a while");
+    createCard("helloween.png", "also made as a discord profile picture, although for halloween");
+    createCard("EEEEEEEEAAAAAAAAA.png", "trying out different styles");
+    createCard("NEWYEAR_HOUSE.png", "some interior drawings for a christmas project i am working on");
+});
+
+const getCard = (filename, description) => `<div class="card">
+            <div class="card-content">
+                <a href="assets/img/gallery/${filename}" class="card-img-link"><img class="card-img" src="assets/img/gallery/${filename}"></a>
+                <div class="card-description">
+                    <h3 class="card-filename">${filename}</h3>
+                    <p class="card-description">${description}</p>
+                </div>
+            </div>
+        </div>`;
+
+const createCard = (filename, description) => $(".card-container").append(getCard(filename, description));
---
+++ js/theme.js
@@ -0,0 +1,17 @@
+﻿$(() => {
+    if(localStorage.getItem("theme") == null)
+        localStorage.setItem("theme", "dark");
+    setTheme(localStorage.getItem("theme"));
+    $("#toggle-theme-button").click(() => {
+        const newTheme = localStorage.getItem("theme") === "dark" ? "light": "dark";
+        localStorage.setItem("theme", newTheme);
+        setTheme(newTheme);
+    });
+});
+
+const setTheme = (t) => {
+    const link = $("#gruvbox-theme-link");
+    let source = link.attr("href");
+    source = t === "light" ? "css/gruvbox_light.css": "css/gruvbox.css";
+    link.attr("href", source);  
+};
