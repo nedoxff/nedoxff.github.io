9b03b3cf180205bb422166acac2d592788a445c2
nedoxff <nedoprogrammer0xff@gmail.com>
2022-12-26 23:06:26 +0300

    settings

--- js/art.js
+++ js/art.js
@@ -3,7 +3,11 @@
     createCard("helloween.png", "also made as a discord profile picture, although for halloween");
     createCard("EEEEEEEEAAAAAAAAA.png", "trying out different styles");
     createCard("NEWYEAR_HOUSE.png", "some interior drawings for a christmas project i am working on");
-    $(".card-container").append(`<a href="/projects/anger"><img style="margin-top: 15px" class="card-img" src="assets/img/anger_thumb.png"></a>`);
+    $(".card-container").append(`<div style="display: flex; flex-direction: row; align-content: baseline">
+            <a href="/projects/anger"><img style="margin-top: 15px" class="card-img"
+                                           src="assets/img/anger_thumb.png"></a>
+                <button id="anger-settings-button" style="margin-top: 15px; margin-left: 5px;" onclick="window.location.href = window.location.href.substring(window.location.href.indexOf('art'), null) + 'projects/anger_settings';"><i class="material-icons">settings</i></button>
+            </div>`);
 });
 
 const getCard = (filename, description) => `<div class="card">
